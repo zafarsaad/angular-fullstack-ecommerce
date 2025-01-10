cb2bd37965a426592801b465a8377cf1adbea5b0
Saad Zafar <saadzafar.code@gmail.com>
2025-01-10 15:47:35 +0100

    navbar update, minute-mark 26

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaConfig, FaIconLibrary, FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
-  imports: [RouterModule, FaIconComponent],
+  standalone: true,
+  imports: [RouterModule, FaIconComponent, NavbarComponent, FooterComponent],
   selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,7 @@
 import { IconDefinition } from "@fortawesome/angular-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
-    faUser
+    faUser,
+    faCartShopping,
 ]
