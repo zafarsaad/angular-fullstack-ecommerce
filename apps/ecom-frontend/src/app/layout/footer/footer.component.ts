a7769df6625d9ff2cfb7dd93799d235c810c3216
Saad Zafar <saadzafar.code@gmail.com>
2025-01-10 17:16:29 +0100

    footer update, checkpoint 1

--- apps/ecom-frontend/src/app/layout/footer/footer.component.ts
+++ apps/ecom-frontend/src/app/layout/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,12 @@
 import { IconDefinition } from "@fortawesome/angular-fontawesome";
-import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faCartShopping, faTruckFast, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
     faUser,
     faCartShopping,
+    faTruckFast,
+    faYoutube,
+    faFacebook,
+    faTwitter
 ]
